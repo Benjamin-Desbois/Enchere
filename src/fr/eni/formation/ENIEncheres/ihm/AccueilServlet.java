eb6ddc922f83e1db508df3d4aac910a30fcb3f40
Michel Dupouy <michel.dupouy2021@campus-eni.fr>
2021-08-26 09:33:41 +0200

    Merge

--- src/fr/eni/formation/ENIEncheres/ihm/AccueilServlet.java
+++ src/fr/eni/formation/ENIEncheres/ihm/AccueilServlet.java
@@ -50,26 +50,27 @@ public class AccueilServlet extends HttpServlet {
 		}
 
 		try {
-			for (Article art : manager.getAllArticles()) {
-				
-				model.setArticle(manager.getArticleById((Integer) art.getNoArticle()));
-				String str1 = "nomarticle";
-				String srt2 = String.valueOf(i);
-				String result = str1+srt2;
-				request.setAttribute(result, model.getArticle().getNomArticle());
-				str1 = "datefin";
-				result = str1+srt2;
-				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-				request.setAttribute(result, model.getArticle().getDateFinEncheres().format(formatter));
-				str1 = "prix";
-				result = str1+srt2;
-				request.setAttribute(result, model.getArticle().getMiseAPrix());
-				str1 = "nomutilisateur";
-				result = str1+srt2;
-				request.setAttribute(result, model.getArticle().getVendeur().getPseudo());
-				i++;
-				
-			}
+		
+			request.setAttribute("lstArticle", manager.getAllArticles());
+//			for (Article art : manager.getAllArticles()) {
+//				
+//				model.setArticle(manager.getArticleById((Integer) art.getNoArticle()));
+//				String str1 = "nomarticle";
+//				String srt2 = String.valueOf(i);
+//				String result = str1+srt2;
+//				request.setAttribute(result, model.getArticle().getNomArticle());
+//				str1 = "datefin";
+//				result = str1+srt2;
+
+//				str1 = "prix";
+//				result = str1+srt2;
+//				request.setAttribute(result, model.getArticle().getMiseAPrix());
+//				str1 = "nomutilisateur";
+//				result = str1+srt2;
+//				request.setAttribute(result, model.getArticle().getVendeur().getPseudo());
+//				i++;
+//				
+//			}
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
